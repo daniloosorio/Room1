22b8187e4fceea45df020c777a40b292f3d812b1
Danilo Osorio Higuita <odanilo38@gmail.com>
2020-07-08 21:08:21 -0500

    comit y push de firebase

--- app/src/main/java/com/daniloosorio/room/LoginActivity.kt
+++ app/src/main/java/com/daniloosorio/room/LoginActivity.kt
@@ -1,15 +1,41 @@
 package com.daniloosorio.room
 
+import android.R.attr.password
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+         val mAuth : FirebaseAuth = FirebaseAuth.getInstance()
         bt_entrar.setOnClickListener {
+            val email =et_email.text.toString()
+            val password =et_password.text.toString()
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    this
+                ) { task ->
+                    if (task.isSuccessful) {
+                       startActivity(Intent(this,MainActivity::class.java))
+                    } else {
+                        Toast.makeText(
+                            this, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        Log.w("TAG", "signInWithEmail:failure", task.getException());
+
+                    }
+
+                    // ...
+                }
             val intent= Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
--- app/src/main/java/com/daniloosorio/room/RegistroActivity.kt
+++ app/src/main/java/com/daniloosorio/room/RegistroActivity.kt
@@ -1,15 +1,41 @@
 package com.daniloosorio.room
 
+import android.R.attr.password
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_registro.*
+
 
 class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        val mAuth : FirebaseAuth = FirebaseAuth.getInstance()
         bt_guardar.setOnClickListener {
+            val email =et_Email.text.toString()
+            val password = et_contrasena.text.toString()
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    this
+                ) { task ->
+                    if (task.isSuccessful) {
+                        crearUsuarioEnBaseDeDatos()
+                       onBackPressed()
+                    } else {
+
+                        Toast.makeText(
+                            this, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        )
+                    }
+
+                    // ...
+                }
             if(et_nombre.text.isNullOrEmpty() || et_Email.text.isNullOrEmpty()||et_contrasena.text.isNullOrEmpty()|| et_Rcontrasena.text.isNullOrEmpty()||sp_pais.selectedItem.toString()=="Seleccione un pais"){
                 
             }
@@ -17,5 +43,9 @@ class RegistroActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun crearUsuarioEnBaseDeDatos() {
+        TODO("Not yet implemented")
     }
 }
